f8d64b620b7e3bfe6e27b0bda6ab04710d3e42c0
leegivhan <73129933+leegivhan@users.noreply.github.com>
2021-08-17 18:45:45 -0400

    recursion: collatz compiled

--- wk03/shorts/collatz-recursion.c
+++ wk03/shorts/collatz-recursion.c
@@ -2,19 +2,21 @@
 
 int collatz(int n);
 
-int main(void)
-{
-    int positive_integer = 1;
-    int steps = collatz(positive_integer);
-    printf("It took %i steps to get to 1.\n", steps);
-    return 0;
-}
+// int main(void)
+// {
+//     int positive_integer = 1;
+//     int steps = collatz(positive_integer);
+//     printf("It took %i step(s) to get to 1.\n", steps);
+//     return 0;
+// }
 
 int collatz(int n)
 {
     if (n == 1)
+    {
         return 1;
-    if (n % 2 == 0)
-        
-
+    }
+    /* exit case after doing checks so it won't return null 
+    which is void data type */
+    return 0;
 }
